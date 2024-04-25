fb93906a89edd4c14c651e2b9fc038b3812bdc7b
randy5452728 <randy@wit.id>
2024-04-25 11:31:36 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php include("header.php"); ?>
+
+<section class="section-cover">
+    <div class="container">
+        <div class="container-frame" style="background-image: url('assets/img/bg.webp'); ">
+            <div class="frame">
+                <div class="tl-frame">
+                    <img src="assets/img/frame-tl.webp" alt="" srcset="">
+                </div>
+                <div class="tr-frame">
+                    <img src="assets/img/frame-tr.webp" alt="" srcset="">
+                </div>
+                <div class="bl-frame">
+                    <img src="assets/img/frame-bl.webp" alt="" srcset="">
+                </div>
+                <div class="br-frame">
+                    <img src="assets/img/frame-br.webp" alt="" srcset="">
+                </div>
+            </div>
+            <div class="content-cover" style="background-image: url('assets/img/circle.webp');">
+                <div class="content-name">
+                    <h1>Subawa
+                        <br>
+                        <span>and</span>
+                        <br>
+                        Erlangga
+                    </h1>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<?php include("footer.php");  ?>
